bd4a7dae8119d2d4fe2fe7d565b1bd4c3a7c0c5f
V5809 (Course ID) <v5809@cam.ac.uk>
2015-08-13 14:07:08 +0100

    Photo editing catz

--- module1.js
+++ module1.js
@@ -29,11 +29,116 @@ function getRandomInt(min, max) {
 // function definitions here
 
 
-$(document).ready(function() {
-    var img = new Image();
-    img.src = "img/cat.jpg";
+$(document).ready(
+    function() {
+        var img = new Image();
+        img.src = "img/cat.jpg";
+        //var pixels = ImageUtils.getPixels(img);
+        //console.log(pixels);
+        //var pixels = ImageUtils.getPixels(img);
+        //var heightPixelsToHide = 150;
+        //for(var i = 0; i < img.width * heightPixelsToHide * 4; i++){
+        //pixels.data[i] =255;
+
+        //intensify(img, -100, "all");
+        //});
+        //ImageUtils.putPixels(pixels, img.width, img.height);
+        // }
+        //makeInvert(img);
+
+        makeFunky(img);
+       // makeNoise(img, 50);
+    }
+);
+
+function intensify(img, adjustment, colour){
+    var pixels = ImageUtils.getPixels(img);
+    var length = pixels.data.length;
+    var data = pixels.data;
+
+    if (colour  == "red" || colour == "all") {
+        for (var i = 0; i < length; i += 4) {
+            data[i] = data[i] + adjustment;
+        }
+
+    }
+
+    if (colour == "green" || colour == "all"){
+        for (var i = 0; i < length; i += 4) {
+            data[i+1] = data[i+1] + adjustment;
+        }
+
+    }
+
+    if (colour == "blue" || colour == "all") {
+        for (var i = 0; i < length; i += 4) {
+            data[i+2] = data[i+2] + adjustment;
+        }
+    }
+    //for (var i = 0; i < length; i += 4) {
+           // data[i+2] = data[i+2] + adjustment;
+    //}
+    ImageUtils.putPixels(pixels, img.width, img.height);
+}
+
+/*function brighten(img, adjustment) {
+    var pixels = ImageUtils.getPixels(img);
+    var length = pixels.data.length;
+    var data = pixels.data;
+
+        for (var i = 0; i < length; i += 1) {
+            data[i] = data[i] + adjustment;
+        }
 
 
 
 
-});
+    ImageUtils.putPixels(pixels, img.width, img.height);
+}*/
+
+function makeInvert(img) {
+    var pixels = ImageUtils.getPixels(img);
+    var length = pixels.data.length;
+    var data = pixels.data;
+
+    for (var i = 0; i < length; i += 4) {
+        data[i] = 255 - data[i];
+        data[i+1] = 255 - data[i+1];
+        data[i+2] = 255 - data[i+2];
+    }
+
+
+    ImageUtils.putPixels(pixels, img.width, img.height);
+}
+
+function makeNoise(img, level) {
+
+    var pixels = ImageUtils.getPixels(img);
+    var length = pixels.data.length;
+    var data = pixels.data;
+
+    for (var i = 0; i < length; i += 4) {
+
+        var adjustment = getRandomInt(-level, level);
+
+        data[i] = data[i] + adjustment;
+        data[i + 1] = data[i + 1] + adjustment;
+        data[i + 2] = data[i + 2] + adjustment;
+
+    }
+        ImageUtils.putPixels(pixels, img.width, img.height);
+
+}
+
+function makeFunky(img) {
+    var pixels = ImageUtils.getPixels(img);
+    var length = pixels.data.length;
+    var data = pixels.data;
+
+    for (var i = 0; i < length/2; i += 2) {
+        var temp = data[i];
+        data[i] = data[length - i];
+        data[length - i] = temp;
+    }
+    ImageUtils.putPixels(pixels, img.width, img.height);
+}
